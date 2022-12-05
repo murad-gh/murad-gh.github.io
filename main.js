b513b5f8169f07fdc83825d5d9adfaf8b37407a1
murad-gh <116010283+murad-gh@users.noreply.github.com>
2022-12-06 00:12:08 +0300

    06.12

--- main.js
+++ main.js
@@ -68,3 +68,31 @@ const mobile_header = document.querySelector(".mobile-header");
 mobile_img.addEventListener("click", () => {
     mobile_header.classList.toggle("active");
 })
+
+// mobile div menu About Me clicked hide 
+
+const mobile_div_menu_nav_a1 = document.querySelector(".mobile-div-menu-nav-a1");
+const mobile_div_menu_nav =document.querySelector("mobile-div-menu-nav");
+
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_div_menu_nav.classList.toggle("hide");
+})
+
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_div_menu.classList.toggle("active");
+})
+
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_menu_line1.classList.toggle("active");
+})
+
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_menu_line2.classList.toggle("active");
+})
+
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_div_gh.classList.toggle("active");
+})
+mobile_div_menu_nav_a1.addEventListener("click", () => {
+    mobile_div_lang_logo.classList.toggle("active");
+})
